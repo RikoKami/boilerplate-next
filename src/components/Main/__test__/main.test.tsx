315f28ac342d1e11139517ae4b4e4a8b18785929
Riko <orglary@gmail.com>
2021-05-13 19:21:33 -0300

    test: first tested component when verifying render the title

---
+++ src/components/Main/__test__/main.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react"
+import Main from ".."
+
+describe("<Main />", () => {
+	it("Should render the heading", () => {
+		render(<Main />)
+
+		expect(
+			screen.getByRole("heading", { name: /react avançado/gi })
+		).toBeInTheDocument()
+	})
+})
