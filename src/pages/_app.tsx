d0b3f814ff379add8f456c5f4225159fbc1ec16a
Riko <orglary@gmail.com>
2021-05-15 19:27:37 -0300

    feat: add favicon with meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,12 @@ function App({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<title>Boilerplate</title>
+				<link rel="shortcut icon" href="/img/icon-512.png" />
+				<link rel="apple-touch-icon" href="/img/icon-512.png" />
+				<meta
+					name="description"
+					content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
