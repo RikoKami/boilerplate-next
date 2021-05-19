b9ad03aabb31ef9f72242cded64799c9b2bec1b9
Riko <orglary@gmail.com>
2021-05-18 23:42:21 -0300

    wip: adding storebook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+		"@storybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
