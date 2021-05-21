7557bbf92c3d4165f622ea668cb3e8940d824f61
Riko <orglary@gmail.com>
2021-05-20 22:13:46 -0300

    fix: removing addons to keep only addon-essentials as it will be deprecated in version 7 of the storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,5 @@ module.exports = {
   "stories": [
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register"
-  ]
+  "addons": ["@storybook/addon-essentials"]
 }
